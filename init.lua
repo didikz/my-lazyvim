f14cbb022417407fd4a5b3e13e7b0c560a791c74
Didik Tri Susanto <rezd14@gmail.com>
2026-07-21 09:41:30 +0700

    docs: clarify intelephense choice in init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
-vim.g.lazyvim_php_lsp = "intelephense"
+vim.g.lazyvim_php_lsp = "intelephense" -- LSP for completion/hover/diagnostics; phpactor handles refactor via lua/plugins/php.lua
 require("config.lazy")
